083b764b2ec37c878da522505a42cab921fc387d
taehyunKim <office.div1201@gmail.com>
2021-06-13 23:14:54 +0900

    Modify: 필드 모듈화

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 "use strict";
 import PopUp from "./popup.js";
-const CARROT_SIZE = 80;
+import Field from "./field.js";
+
 const CARROT_COUNT = 20;
 const BUG_COUNT = 20;
 const GAME_DURATION_SEC = 20;
 
-const field = document.querySelector(".game__field");
-const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector(".game__button");
 const timerIndicator = document.querySelector(".game__timer");
 const gameScore = document.querySelector(".game__score");
@@ -24,7 +23,24 @@ let timer = undefined;
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListener(() => startGame());
 
-field.addEventListener("click", onFieldClick);
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickLiener(onItemClick);
+
+function onItemClick(item) {
+  if (!started) {
+    return;
+  }
+  if (item === "carrot") {
+    score++;
+    updateScoreBoard();
+    if (score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if (item === "bug") {
+    finishGame(false);
+  }
+}
+
 gameBtn.addEventListener("click", () => {
   if (started) {
     stopGame();
@@ -105,28 +121,8 @@ function updateTimerText(time) {
 
 function initGame() {
   score = 0;
-  field.innerHTML = "";
   gameScore.innerText = CARROT_COUNT;
-  addItem("carrot", CARROT_COUNT, "img/carrot.png");
-  addItem("bug", BUG_COUNT, "img/bug.png");
-}
-
-function onFieldClick(event) {
-  if (!started) {
-    return;
-  }
-  const target = event.target;
-  if (target.matches(".carrot")) {
-    target.remove();
-    score++;
-    playSound(carrotSound);
-    updateScoreBoard();
-    if (score === CARROT_COUNT) {
-      finishGame(true);
-    }
-  } else if (target.matches(".bug")) {
-    finishGame(false);
-  }
+  gameField.init();
 }
 
 function playSound(sound) {
@@ -140,26 +136,4 @@ function stopSound(sound) {
 
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
-}
-
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width - CARROT_SIZE;
-  const y2 = fieldRect.height - CARROT_SIZE;
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement("img");
-    item.setAttribute("class", className);
-    item.setAttribute("src", imgPath);
-    item.style.position = "absolute";
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
-    field.appendChild(item);
-  }
-}
-
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
 }
--- src/popup.js
+++ src/popup.js
@@ -5,7 +5,7 @@ export default class PopUp {
     this.popUpRefresh = document.querySelector(".pop-up__refresh");
     this.popUpRefresh.addEventListener("click", () => {
       this.onClick && this.onClick();
-      hide();
+      this.hide();
     });
   }
 
