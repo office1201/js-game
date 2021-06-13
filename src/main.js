809ebdd2559b498663be8d8a99df18b8f28ffb21
taehyunKim <office.div1201@gmail.com>
2021-06-13 18:34:09 +0900

    Add: 타이머 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const VEGETABLE_SIZE = 80;
 const VEGETABLE_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
@@ -39,6 +40,24 @@ function showStopButton() {
 function showTimerAndScore() {
   gameTimer.style.visibility = "visible";
   gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
 }
 
 const initGame = () => {
