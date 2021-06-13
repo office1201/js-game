1010c3d395d0f415575bb48ed237edcdf984219e
taehyunKim <office.div1201@gmail.com>
2021-06-13 18:27:57 +0900

    Add: 게임 시작 시 아이템 및 카운트 생성

--- src/main.js
+++ src/main.js
@@ -1,11 +1,51 @@
 const VEGETABLE_SIZE = 80;
+const VEGETABLE_COUNT = 5;
+const BUG_COUNT = 5;
 
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame;
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
 
 const initGame = () => {
-  addItem("carrot", 5, "../img/vegetable.png");
-  addItem("bug", 5, "../img/bug.png");
+  field.innerHTML = "";
+  gameScore.innerText = VEGETABLE_COUNT;
+  addItem("carrot", VEGETABLE_COUNT, "../img/vegetable.png");
+  addItem("bug", BUG_COUNT, "../img/bug.png");
 };
 
 const addItem = (className, count, imgPath) => {
@@ -29,5 +69,3 @@ const addItem = (className, count, imgPath) => {
 const randomNumber = (min, max) => {
   return Math.random() * (max - min) + min;
 };
-
-initGame();
