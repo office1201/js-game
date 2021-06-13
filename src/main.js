4c404e38560de3090d831432a90f20540d75872a
taehyunKim <office.div1201@gmail.com>
2021-06-13 18:51:52 +0900

    Add: 게임 정지 구현

--- src/main.js
+++ src/main.js
@@ -9,13 +9,17 @@ const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
 const gameScore = document.querySelector(".game__score");
 
+const popUp = document.querySelector(".pop-up");
+const popUpText = document.querySelector(".pop-up__message");
+const popUpRefresh = document.querySelector(".pop-up__refresh");
+
 let started = false;
 let score = 0;
 let timer = undefined;
 
 gameBtn.addEventListener("click", () => {
   if (started) {
-    stopGame;
+    stopGame();
   } else {
     startGame();
   }
@@ -29,12 +33,20 @@ function startGame() {
   startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText("REPLAY❓");
+}
 
 function showStopButton() {
   const icon = gameBtn.querySelector(".fa-play");
   icon.classList.add("fa-stop");
   icon.classList.remove("fa-play");
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = "hidden";
 }
 
 function showTimerAndScore() {
@@ -54,10 +66,19 @@ function startGameTimer() {
   }, 1000);
 }
 
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
 function updateTimerText(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove("pop-up--hide");
 }
 
 const initGame = () => {
